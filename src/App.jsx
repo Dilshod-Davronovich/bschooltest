e7556598cb2fc83f4f0a9c58b402b0031fa77f55
Shohjahon Zubaydullayev <zubaydullayevshohjahon4@gmail.com>
2025-01-13 21:18:54 +0500

    Firebase connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { getDatabase, ref, set } from "firebase/database"
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+
+  function writeUserData(id, testlar) {
+    // const db = getDatabase();
+    set(ref(database, 'tests/' + `${id}`), {
+      testlar: testlar
+    });
+  }
+  const birinchiTest = [
+    {savol:"apple nima?",a:"olcha",b:"gilos",c:"olma",javob:"olma"},
+    {savol:"Get nima?",a:"olmoq",b:"bermoq",c:"ketmoq",javob:"olmoq"}
+  ]
+  const test=JSON.stringify(birinchiTest)
+  writeUserData(2, test)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBVOwHCCEm_kWdo1U95J8Ge7rvtE8mstDo",
+    authDomain: "bschool-fc29b.firebaseapp.com",
+    databaseURL: "https://bschool-fc29b-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "bschool-fc29b",
+    storageBucket: "bschool-fc29b.firebasestorage.app",
+    messagingSenderId: "256321866906",
+    appId: "1:256321866906:web:4445c597acf54ed2bee323"
+};
+
+
+
+export default firebaseConfig;
