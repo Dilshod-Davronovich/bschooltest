e46efbdf63f4c8d63d6e63373493d97041bbe2af
Shohjahon Zubaydullayev <zubaydullayevshohjahon4@gmail.com>
2025-01-14 16:16:12 +0500

    design completed

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,10 +4,11 @@ const Welcome = () => {
   return (
     <>
         <header>
-            <h1>BobobekovSchool ga xush kelibsiz!</h1>
+            <h1>Welcome to BobobekovSchool!</h1>
         </header>
         <main>
             <input type="search" />
+            <button>Search</button>
         </main>
     </>
   )
