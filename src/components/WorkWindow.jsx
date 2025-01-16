83e49562f3663bcb52ba801536d8ab2a05cc8686
Shohjahon Zubaydullayev <zubaydullayevshohjahon4@gmail.com>
2025-01-16 21:16:32 +0500

    8

--- src/components/WorkWindow.jsx
+++ src/components/WorkWindow.jsx
@@ -1,17 +1,36 @@
 import { useState } from 'react';
 import testsData from './data';
+import styles from './WorkWindow.module.css'
 
 function WorkWindow() {
-    const [testName, setTestName] = useState(testsData[0]);
-    const [question, setQuestion] = useState(testsData[1]);
-    
-    console.log(question);
-    
+    const [testName] = useState(testsData[0]);
+    const [questions] = useState(testsData[1]);
+
+    function check(answer, correctAnswer) {
+        if (answer === correctAnswer) {
+            alert("To'g'ri");
+        } else {
+            alert("Xato");
+        }
+    }
+
     return (
-        <>
+        <div className={styles.container}>
             <h1>{testName}</h1>
-        </>
-    )
+            <ol>
+                {questions.map((question, index) => (
+                    <li key={index}>
+                        <p>{question.savol}</p>
+                        <ul>
+                            <li onClick={() => check(question.a, question.javob)}>{question.a}</li>
+                            <li onClick={() => check(question.b, question.javob)}>{question.b}</li>
+                            <li onClick={() => check(question.c, question.javob)}>{question.c}</li>
+                        </ul>
+                    </li>
+                ))}
+            </ol>
+        </div>
+    );
 }
 
 export default WorkWindow;
