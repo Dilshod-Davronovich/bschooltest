5b354bf1171429c2b875f00e740472c92df0e94c
Bobobekov Dilshod <tvbunima@gmail.com>
2025-01-28 00:07:02 +0500

    Shohjahon All Tests complated

---
+++ src/components/AdminPanel.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import AddTest from './AddTest';
+import styles from './AdminPanel.module.css';
+import kunImg from '../assets/kun.png';
+import kechImg from '../assets/kech.png';
+
+const AdminPanel = () => {
+   const [changeBgrounds, setChangeBgrounds] = useState(true);
+
+   function changeBg() {
+      if (changeBgrounds) {
+         document.documentElement.style.setProperty(
+            '--main-body-color',
+            '#343434'
+         );
+         document.documentElement.style.setProperty(
+            '--main-bg-color',
+            '#434343'
+         );
+         document.documentElement.style.setProperty(
+            '--dynamic-text-color',
+            '#E9E5E2'
+         );
+         document.documentElement.style.setProperty(
+            '--dynamic-inputs-color',
+            '#6E6E6F'
+         );
+         document.documentElement.style.setProperty(
+            '--dynamic-img',
+            `url(${kunImg})`
+         );
+         setChangeBgrounds(false);
+      } else {
+         document.documentElement.style.setProperty(
+            '--main-body-color',
+            'white'
+         );
+         document.documentElement.style.setProperty(
+            '--main-bg-color',
+            'lightcyan'
+         );
+         document.documentElement.style.setProperty(
+            '--dynamic-text-color',
+            'black'
+         );
+         document.documentElement.style.setProperty(
+            '--dynamic-inputs-color',
+            'white'
+         );
+         document.documentElement.style.setProperty(
+            '--dynamic-img',
+            `url(${kechImg})`
+         );
+         setChangeBgrounds(true);
+      }
+   }
+
+   return (
+      <div>
+         <header className={styles.headerWindow}>
+            <h1>ADMINSTRATOR BO`LIMI</h1>
+            <button
+               onClick={changeBg}
+               className={styles.dayNightButton}
+            ></button>
+         </header>
+         <AddTest />
+      </div>
+   );
+};
+
+export default AdminPanel;
